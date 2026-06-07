6d1c321787dc927f81c5a538e7109f7ad264a12b
Pedro Cabral <p.cabral@flamus.com>
2026-06-06 22:10:33 -0300

    feat: add mobile decorative image below hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,23 @@ export default function Home() {
       <Header />
       <div className="relative flex-1 flex flex-col">
         <HeroSection />
+        {/* Desktop decorative image */}
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           src="/elements/decoration/footer.png"
           alt=""
-          className="absolute bottom-0 left-0 w-full h-auto block pointer-events-none"
+          className="absolute bottom-0 left-0 w-full h-auto pointer-events-none hidden md:block"
         />
+        {/* Mobile decorative image */}
+        <div className="block md:hidden mt-[10px] mx-[30px]">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="/elements/decoration/main/rabeta.webp"
+            alt=""
+            className="w-full pointer-events-none"
+            style={{ transform: 'scaleX(-1)' }}
+          />
+        </div>
       </div>
       <Footer />
     </>
